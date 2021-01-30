d9052f260a933523ecba73c9934e97540422ca9a
unknown <sungs201@naver.com>
2021-01-30 17:18:22 +0900

    2021/01/30 1문제 해결, 1문제 못품었음.

--- study.cpp
+++ study.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <queue>
 #include <stack>
+#include <set>
 #pragma warning(disable:4996)
 using namespace std;
 
@@ -183,3 +184,188 @@ int main() {
 	cout << maxSize << "\n";
 	cout << maxSumSize << "\n";
 }*/
+
+/*int w[100], v[100], cache[101][100001]; //12865 평범한 배낭
+int main() {
+	int i, j, n, k;
+	cin >> n >> k;
+	for (i = 1; i <= n; i++) {
+		cin >> w[i] >> v[i];
+	}
+
+	int ret = 0;
+
+	for (i = 1; i <= n; i++) {
+		for (j = 0; j <= k; j++) {
+			cache[i][j] = cache[i - 1][j];
+			if (j - w[i] >= 0) {
+				cache[i][j] = max(cache[i][j], cache[i - 1][j - w[i]] + v[i]);
+				ret = max(ret, cache[i][j]);
+			}
+		}
+	}
+
+	cout << ret;
+}*/
+
+/*int s2d2[11][11], soil[11][11], num, adj[8][2] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1} };
+priority_queue<int> board[11][11]; // 각 나무의 나이 저장
+vector<int> dead[11][11];
+int main() {
+	int n, m, k, p, q, r, s, x, y, z;
+	cin >> n >> m >> k;
+	num = m; // 나무 갯수 저장
+	for (p = 1; p <= n; p++) {
+		for (q = 1; q <= n; q++) {
+			cin >> s2d2[p][q];
+			soil[p][q] = 5;
+		}
+	}
+
+	for (p = 0; p < m; p++) {
+		cin >> x >> y >> z;
+		board[x][y].push(z);
+	}
+
+	for (p = 0; p < k; p++) {
+		// 봄
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				vector<int> tmp;
+				while (!board[q][r].empty()) {
+					int tree = board[q][r].top();
+					board[q][r].pop();
+					if (soil[q][r] >= tree) { // 나무 생존
+						soil[q][r] -= tree;
+						tree++;
+						tmp.push_back(tree);
+					}
+					else { // 나무 사망
+						dead[q][r].push_back(tree);
+						num--;
+					}
+				}
+				for (auto it : tmp) {
+					board[q][r].push(it);
+				}
+			}
+		}
+		// 여름
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				for (auto it : dead[q][r]) {
+					soil[q][r] += it / 2;
+				}
+				dead[q][r].clear();
+			}
+		}
+
+		// 가을
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				vector<int> tmp2;
+				while (!board[q][r].empty()) {
+					int tree = board[q][r].top();
+					board[q][r].pop();
+					if (tree % 5 == 0) { // 번식
+						for (s = 0; s < 8; s++) {
+							int nxtQ = q + adj[s][0], nxtR = r + adj[s][1];
+							if (nxtQ<1 || nxtQ>n || nxtR<1 || nxtR>n) continue;
+							board[nxtQ][nxtR].push(1); // 새 나무 추가
+							num++; // 새 나무 추가
+						}
+					}
+					tmp2.push_back(tree);
+				}
+				for (auto it : tmp2) board[q][r].push(it);
+			}
+		}
+
+		//겨울
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				soil[q][r] += s2d2[q][r];
+			}
+		}
+	}
+
+	cout << num;
+}*/
+
+int s2d2[11][11], soil[11][11], num, adj[8][2] = { {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1} };
+vector<int> board[11][11]; // 각 나무의 나이 저장
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m, k, p, q, r, s, x, y, z;
+	cin >> n >> m >> k;
+	num = m; // 나무 갯수 저장
+	for (p = 1; p <= n; p++) {
+		for (q = 1; q <= n; q++) {
+			cin >> s2d2[p][q];
+			soil[p][q] = 5;
+		}
+	}
+
+	for (p = 0; p < m; p++) {
+		cin >> x >> y >> z;
+		board[x][y].push_back(z);
+	}
+
+	for (p = 0; p < k; p++) {
+		// 봄
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				for (auto& tree : board[q][r]) {
+					if (tree < 0) continue;
+					if (soil[q][r] >= tree) { // 나무 생존
+						soil[q][r] -= tree;
+						tree++;
+					}
+					else { // 나무 사망
+						tree = -tree; // 여름에 처리하기 위해 음수만 붙임. 처리 끝나면 -999로 조정
+						num--;
+					}
+				}
+			}
+		}
+
+		// 여름
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				for (auto& tree : board[q][r]) {
+					if (tree < 0 && tree != -999) {
+						soil[q][r] += (-tree) / 2;
+						tree = -999;
+					}
+				}
+			}
+		}
+
+		// 가을
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				for (auto& tree : board[q][r]) {
+					if (tree % 5 == 0) { // 번식
+						for (s = 0; s < 8; s++) {
+							int nxtQ = q + adj[s][0], nxtR = r + adj[s][1];
+							if (nxtQ<1 || nxtQ>n || nxtR<1 || nxtR>n) continue;
+							board[nxtQ][nxtR].push_back(1); // 새 나무 추가
+							sort(board[nxtQ][nxtR].begin(), board[nxtQ][nxtR].end()); // 나이 어린 순으로 정렬
+							num++; // 새 나무 추가
+						}
+					}
+				}
+			}
+		}
+
+		//겨울
+		for (q = 1; q <= n; q++) {
+			for (r = 1; r <= n; r++) {
+				soil[q][r] += s2d2[q][r];
+			}
+		}
+	}
+
+	cout << num;
+}
